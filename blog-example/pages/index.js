85095603c0497df2da143f70a7b625250da5f0ae
nwnp <jjw_0418@naver.com>
2024-01-02 20:58:30 +0900

    SSG와 SSR 예시

--- blog-example/pages/index.js
+++ blog-example/pages/index.js
@@ -1,8 +1,38 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+/** SSG */
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+/** SSR */
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:3000/api/posts");
+  const json = await response.json();
+  return {
+    props: {
+      allPostsData: json.allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  // const [allPostsData, setAllPostsData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/api/posts")
+  //     .then((res) => console.log(res))
+  //     .then((data) => setAllPostsData(data.allPostsData));
+  // }, []);
+
   return (
     <Layout home>
       <Head>
@@ -14,6 +44,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
